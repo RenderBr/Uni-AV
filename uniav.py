25e64a1f09823c5faf0a7ac0f5ac69b0afdfc5cb
Render <julianmuchacho666@gmail.com>
2017-05-20 13:23:50 -0400

    makin' some new stuff

--- uniav.py
+++ uniav.py
@@ -10,19 +10,31 @@ top = N
 left = W
 bottom = S
 
+autoremove = 0
+
 root = Tk()
 
 #Main frame
 mainframe = Frame(root, height=500, bg="black", relief=SUNKEN)
 mainframe.pack()
 
-#Configuring root
+#Configuring root and Top Window
 root.wm_title("Uni Antivirus")
 root.resizable(0,0)
 
 def quickScan():
     print("Scanning")
     tkinter.messagebox.showinfo("Scan complete", "The scan has been completed!")
+
+def optionsWindow():
+    options = Toplevel()
+
+    autoRemove = Checkbutton(options, text="Automatically remove found threats.", variable=autoremove)
+    autoRemove.pack()
+
+    options.resizable(0,0)
+    options.wm_title("Settings")
+
 
 
 title = Label(mainframe, text="Uni Antivirus", fg="white", bg="black")
@@ -32,6 +44,8 @@ runQuickScan = Button(mainframe, text="Quick Scan", command=quickScan, fg="white
 runQuickScan.pack(side=LEFT)
 runDirScan = Button(mainframe, text="Scan in a certain directory", command=quickScan, fg="white", bg="gray")
 runDirScan.pack(side=LEFT)
+settingsButton = Button(mainframe, text="Settings", command=optionsWindow, fg="white", bg="gray")
+settingsButton.pack(side=LEFT)
 
 
 
