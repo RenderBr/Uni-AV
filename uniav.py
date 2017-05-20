12c56bb42b44302286fd39ce999ebf01d8025b3c
Render <julianmuchacho666@gmail.com>
2017-05-20 12:44:39 -0400

    oops i forgot to commit this

--- uniav.py
+++ uniav.py
@@ -6,23 +6,32 @@ import tkinter.messagebox
 
 #changing compass directions bcuz it's annoying
 right = E
-up = N
+top = N
 left = W
-down = S
+bottom = S
 
 root = Tk()
 
-
+#Main frame
+mainframe = Frame(root, height=500, bg="black", relief=SUNKEN)
+mainframe.pack()
 
 #Configuring root
-root.wm_title("Alpha Antivirus")
+root.wm_title("Uni Antivirus")
+root.resizable(0,0)
 
 def quickScan():
     print("Scanning")
     tkinter.messagebox.showinfo("Scan complete", "The scan has been completed!")
 
-runScan = Button(root, text="Quick Scan", command=quickScan)
-runScan.pack(side=TOP)
+
+title = Label(mainframe, text="Uni Antivirus", fg="white", bg="black")
+title.config(font=("Arial", 44))
+title.pack(side=TOP)
+runQuickScan = Button(mainframe, text="Quick Scan", command=quickScan, fg="white", bg="gray")
+runQuickScan.pack(side=LEFT)
+runDirScan = Button(mainframe, text="Scan in a certain directory", command=quickScan, fg="white", bg="gray")
+runDirScan.pack(side=LEFT)
 
 
 
