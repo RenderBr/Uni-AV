54594c1b0748ab7357e095e1af64d566efa7b950
Render <julianmuchacho666@gmail.com>
2017-05-20 11:19:04 -0400

    Wow a button!
    Added a button and made everything work-ish

--- Engine/k2.py
+++ Engine/k2.py
@@ -1,3 +1,33 @@
-﻿# This is a fork of Kicomav, an antivirus made in Python.
+﻿from tkinter import *
+import tkinter.messagebox
+
+# This is a fork of Kicomav, an antivirus made in Python.
 # New Author: RenderBr
 
+
+
+#changing compass directions bcuz it's annoying
+right = E
+up = N
+left = W
+down = S
+
+root = Tk()
+
+
+
+#Configuring root
+root.wm_title("Alpha Antivirus")
+
+def quickScan():
+    print("Scanning")
+    tkinter.messagebox.showinfo("Scan complete", "The scan has been completed!")
+
+runScan = Button(root, text="Quick Scan", command=quickScan)
+runScan.pack(side=TOP)
+
+
+
+
+
+root.mainloop()
